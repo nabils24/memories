51e8bd5d9e3f3d6b18bc8b0175f66929b2f152c6
Nabil Sahsada Suratno <nabilsahsadacode@gmail.com>
2025-02-21 00:43:16 +0700

    fix: if url updated push to admin page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -280,7 +280,7 @@ export default function SignUp() {
             });
         } finally {
             setIsLoading(false);
-            router.push(`/id/${url}`);
+            router.push(`/admin`);
         }
 
     }
